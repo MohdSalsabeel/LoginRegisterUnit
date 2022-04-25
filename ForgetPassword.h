379c21abf1975d8b1f6eb729c4a73a761a800b8f
MohdSalsabeel <103497056+MohdSalsabeel@users.noreply.github.com>
2022-04-25 18:13:18 +0530

    Create ForgetPassword.h

---
+++ ForgetPassword.h
@@ -0,0 +1,16 @@
+// #ifndef forgettest
+// #define forgettest
+#pragma once
+#include<string>
+//#include "User_module.h"
+
+//#include "login_module.cpp"
+
+
+class ForgetPassword : public User 
+{
+    public:
+    void forget();
+};
+
+//#endif
