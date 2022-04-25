4a3a16add58e677dd515eb551154b6c173626e76
MohdSalsabeel <103497056+MohdSalsabeel@users.noreply.github.com>
2022-04-25 18:15:17 +0530

    Create LoginPage.h

---
+++ LoginPage.h
@@ -0,0 +1,15 @@
+// #ifndef logintest
+// #define logintest
+#pragma once
+#include<string>
+#include "User_module.h"
+//using namespace std;
+
+
+class LoginPage : public User
+{
+    public:
+    std::string login();
+};
+
+//#endif
