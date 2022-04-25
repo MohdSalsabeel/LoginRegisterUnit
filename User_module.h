828b9d5f6525bb7d9fef39d95e60cf446b7bc8ce
MohdSalsabeel <103497056+MohdSalsabeel@users.noreply.github.com>
2022-04-25 18:14:43 +0530

    Create User_module.h

---
+++ User_module.h
@@ -0,0 +1,19 @@
+// #ifndef usertest
+// #define usertest
+#pragma once
+#include<string>
+
+class User
+{
+   public:
+    std::string user_id;
+    std::string password;
+    std::string pin;
+    std::string imei_no;
+    int location;
+    int mobile_no;
+    int time;
+
+};
+
+//#endif
